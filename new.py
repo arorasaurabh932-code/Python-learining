e2c86818ac5aff2ce9c3bed89937a1cf30f4ef89
Saurabh Arora <arorasaurabh932@gmail.com>
2026-06-29 16:59:30 +0530

    Added python negative indexing examples

---
+++ new.py
@@ -0,0 +1,7 @@
+my_skills = ["python", "sql", "tableau", "excel" , "powerbi" ]
+akhri_skill = my_skills[-1]
+print("sabse akhri skill:", akhri_skill)
+second_last = my_skills[-2]
+print("second last skill:" , second_last)
+last_teen = my_skills[-3:]
+print("akhri ki teen skills:", last_teen)
---
+++ test.py
@@ -0,0 +1,9 @@
+first_name = "saurabh"
+last_name = "arora"
+full_name = first_name + '  ' + last_name
+print(full_name)
+my_numbers = [10, 20, 30, 40, 50,]
+print("meri list:" , my_numbers)
+print("peche ka pehla no:",  my_numbers[-1])
+tukda = my_numbers [1:4]
+print (tukda)
