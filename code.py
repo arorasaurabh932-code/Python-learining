3e4f52024218b9b890e08dad4f0191fe7b258be2
Saurabh Arora <arorasaurabh932@gmail.com>
2026-06-29 15:58:50 +0530

    Added python lists and modification basics

---
+++ code.py
@@ -0,0 +1,18 @@
+scores= [95, 80, 88, 75, 92]
+print (scores)     
+print (scores[-1])
+print(scores[1:2])
+print(scores[1:3])
+print(scores[2:4])
+print(scores[2:5])
+print(scores[2:])
+my_skills = ["python", "git", "sql"]
+my_skills[1] = "github"
+print(my_skills)
+tools = ["excel","word","powerbi"]
+tools[1]= "python"
+print (tools) 
+tools.append("sql")
+print(tools)
+tools.insert(0, "tableau")
+print(tools)
